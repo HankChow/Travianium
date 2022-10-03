3113dd78b72a167898357c20f356d5fb4a6091a9
zouchong <zouchong@inconnecting.com>
2022-10-03 12:02:29 +0800

    fix: village list

--- travian.py
+++ travian.py
@@ -162,11 +162,10 @@ class Travian(object):
         info["village_list"] = [{
             "name": _.select("span.name")[0].get_text(),
             "coordinates": {
-                "x": int(_.select("span.coordinatesGrid")[0].get("data-x")),
-                "y": int(_.select("span.coordinatesGrid")[0].get("data-y")),
+                "x": int(_.select("span.coordinatesGrid span.coordinateX")[0].get_text().encode('ascii', 'ignore').decode('unicode_escape').strip("(")),
+                "y": int(_.select("span.coordinatesGrid span.coordinateY")[0].get_text().encode('ascii', 'ignore').decode('unicode_escape').strip(")")),
             },
             "current": "active" in _.get("class")
-        } for _ in village_list]
         info["loyalty"] = soup_dorf1.select(self.selectors["dorf1_loyalty"])[0].get_text().encode('ascii', 'ignore').decode('unicode_escape')
         culture_points = re.findall('\d+\/\d+', soup_dorf1.select("div.expansionSlotInfo")[0].get("title").encode('ascii', 'ignore').decode('unicode_escape'))[-1]
         info["culture_points"] = {
